f3a3929e505396fecf880374851cf945400c0220
Jake Huneycutt <hjhuney@gmail.com>
2019-10-10 05:14:56 -0400

    Updates

---
+++ precache-manifest.12594ef4937b2441fc0756694ec23cb5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcc9e0f8b6ab5eeba51eb872967f71d7",
+    "url": "/React-Neagoie/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "974a4639a4075c0246fb",
+    "url": "/React-Neagoie/monsters-rolodex/static/css/main.76d508a1.chunk.css"
+  },
+  {
+    "revision": "102a9b24fd9babe5b595",
+    "url": "/React-Neagoie/monsters-rolodex/static/js/2.a393edc3.chunk.js"
+  },
+  {
+    "revision": "974a4639a4075c0246fb",
+    "url": "/React-Neagoie/monsters-rolodex/static/js/main.469375e8.chunk.js"
+  },
+  {
+    "revision": "1a4eb354d996907c22d8",
+    "url": "/React-Neagoie/monsters-rolodex/static/js/runtime-main.1b1d29d7.js"
+  }
+]);
